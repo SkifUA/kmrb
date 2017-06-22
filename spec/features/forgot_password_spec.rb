4de7509bee0032c04cbac046068ea17af702e3d1
Valeriy Kuzin <v.kuzin@nixsolutions.com>
2017-06-22 13:36:43 +0300

    RSpec Features ForgotPassword added succes send email

--- spec/features/forgot_password_spec.rb
+++ spec/features/forgot_password_spec.rb
@@ -1,12 +1,22 @@
 require 'spec_helper'
 require "rails_helper"
 
-feature "Login" do
+feature "Forgot password" do
+let(:user) { create :user }
 
   scenario "send message" do
     visit "/password_resets/new"
 
-    fill_in "Email", with: "user@example.com"
+    fill_in "Email", with: user.email
+    click_button "Send"
+
+    expect(page).to have_text(I18n.t 'success_msg_send_reset_mail')
+  end
+
+  scenario "not send message" do
+    visit "/password_resets/new"
+
+    fill_in "Email", with: "example@example.com"
     click_button "Send"
 
     expect(page).to have_text(I18n.t 'error_msg_email_notfound')
