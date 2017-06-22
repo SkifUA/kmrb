bd01fa488a76da3f18ae1d6ec0df9348efc76b83
Valeriy Kuzin <v.kuzin@nixsolutions.com>
2017-06-22 13:30:53 +0300

    RSpec FactoryGirl added gem

---
+++ spec/factories/users.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require "rails_helper"
+
+FactoryGirl.define do
+  factory :user do
+    first_name "Example"
+    last_name 'User'
+    email "user@example.com"
+    password '111111'
+    password_confirmation '111111'
+  end
+end
