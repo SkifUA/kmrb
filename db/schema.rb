999ed7cf9dfcbe1dff5281a73b0688e89150abbf
Valeriy Kuzin <v.kuzin@nixsolutions.com>
2017-06-22 16:43:46 +0300

    User added index for email

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610172956) do
+ActiveRecord::Schema.define(version: 20170622133425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "password_digest"
-    t.string   "remember_digest"
-    t.string   "activation_digest"
-    t.string   "reset_digest"
-    t.boolean  "activated",         default: false
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
+    t.string "remember_digest"
+    t.string "activation_digest"
+    t.string "reset_digest"
+    t.boolean "activated", default: false
     t.datetime "activated_at"
     t.datetime "reset_sent_at"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,3 @@
-require 'spec_helper'
-require "rails_helper"
-
 FactoryGirl.define do
   factory :user do
     first_name "Example"
