a106cf182e63dfa5419fa3b484a3fc939d943a99
Valeriy Kuzin <v.kuzin@nixsolutions.com>
2017-07-03 14:10:03 +0300

    Layout added helper list models

--- app/helpers/admin/base_helper.rb
+++ app/helpers/admin/base_helper.rb
@@ -1,2 +1,8 @@
 module Admin::BaseHelper
+
+  def list_models
+    list = ActiveRecord::Base.connection.tables.map{|x| x.classify}
+    list - ['ArInternalMetadatum', 'SchemaMigration']
+    # ActiveRecord::Base.connection.tables.map{|x|x.classify.safe_constantize}.compact
+  end
 end
