c42b35f6cc702e80c06e3f19585bed3b8d49b3f2
Valeriy Kuzin <v.kuzin@nixsolutions.com>
2017-07-27 20:02:45 +0300

    Admin controller added method edit

--- app/controllers/admin/models_controller.rb
+++ app/controllers/admin/models_controller.rb
@@ -25,6 +25,21 @@ class Admin::ModelsController < Admin::BaseController
   end
 
   def update
+    data_update = model_params
+
+    unless data_update.present?
+      redirect_to admin_edit_model_row_path(@model, params[:id])
+      return
+    end
+
+    @row = @model.find(params[:id])
+    if @row.update_attributes(data_update)
+      flash[:info] = "#{I18n.t 'admin.models.success.update'} #{name_model @model.name}"
+      redirect_to admin_model_row_path(@model, params[:id])
+    else
+      flash[:danger] = "#{I18n.t 'admin.models.errors.update'} #{name_model @model.name}"
+      redirect_to admin_edit_model_row_path(@model, params[:id])
+    end
   end
 
   def destroy
@@ -46,8 +61,20 @@ class Admin::ModelsController < Admin::BaseController
     model::VISIBLE_COLUMNS
   end
 
+  def edible_columns(model)
+    unless defined? model::EDIBLE_COLUMNS
+      flash[:danger] = "#{I18n.t 'admin.models.errors.defined_edible_column'} #{name_model @model}"
+      return []
+    end
+    model::EDIBLE_COLUMNS
+  end
+
   def check_existence_model_form
     lookup_context.find_all("/admin/models/_form_#{@model.name.downcase}").any?
+  end
+
+  def model_params
+    params.require(@model.name.downcase.to_sym).permit(edible_columns @model)
   end
 
 end
