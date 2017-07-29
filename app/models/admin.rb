09c871680440e177098dfbd1e93a28ebe442a496
Valeriy Kuzin <v.kuzin@nixsolutions.com>
2017-07-29 22:43:16 +0300

    Admin Admin added foem for model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,18 @@
 class Admin < ApplicationRecord
   belongs_to :user
   belongs_to :group_admin
+
+  VISIBLE_COLUMNS = [
+      'id',
+      'user_id',
+      'group_admin_id',
+      'created_at'
+  ]
+
+  EDIBLE_COLUMNS = [
+      :user_id,
+      :group_admin_id
+  ]
 # TODO added validate
   # validates :user_id, presence: true
   # validates :group_id, presence: true
