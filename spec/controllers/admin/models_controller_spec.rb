901f69e7d70308d1b277372f05c7bab6a7dde287
Valeriy Kuzin <v.kuzin@nixsolutions.com>
2017-08-03 23:24:42 +0300

    Rspec fix test login and admin_controller

--- spec/controllers/admin/models_controller_spec.rb
+++ spec/controllers/admin/models_controller_spec.rb
@@ -1,12 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Admin::ModelsController, type: :controller do
+  let(:group_admin) { build :group_admin }
+  let(:user) { build :user }
+  let(:admin) { build :admin }
 
-  let(:user) { create :user }
+  before do
+    user.save(validate: false)
+    admin.save(validate: false)
+    group_admin.save(validate: false)
+    session[:user_id] = user.id
+  end
+
 
   describe "GET models#index" do
     it "returns http success" do
-
       get(:index, params: { model: 'user' })
       expect(response).to have_http_status(:success)
     end
@@ -14,7 +22,6 @@ RSpec.describe Admin::ModelsController, type: :controller do
 
   describe "GET models#show" do
     it "returns http success" do
-
       get(:show, params: { model: 'user', id: user.id })
       expect(response).to have_http_status(:success)
     end
